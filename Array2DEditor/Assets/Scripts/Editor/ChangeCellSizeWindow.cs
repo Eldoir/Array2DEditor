aeac8738259d1c3802832a7b2857df0663689168
Arthur Cousseau <arthur.cousseau@me.com>
2021-02-22 15:31:55 +0100

    ChangeCellSizeWindow: autofocus field and apply with Return or KeypadEnter

--- Array2DEditor/Assets/Scripts/Editor/ChangeCellSizeWindow.cs
+++ Array2DEditor/Assets/Scripts/Editor/ChangeCellSizeWindow.cs
@@ -7,24 +7,35 @@ namespace Array2DEditor
     {
         static SerializedProperty cellSizeProperty;
         private static Vector2Int newCellSize;
+
+        private const string cellSizeControlName = "CellSize";
+        private static bool cellSizeControlFocused;
     
         public static void ShowWindow(SerializedProperty cellSizeProperty)
         {
             ChangeCellSizeWindow.cellSizeProperty = cellSizeProperty;
             newCellSize = cellSizeProperty.vector2IntValue;
+
+            cellSizeControlFocused = false;
         
             // Get existing open window or if none, make a new one
-            var window = (ChangeCellSizeWindow)GetWindow(typeof(ChangeCellSizeWindow));
-            // Set the window to be at the center of the screen
-            window.position = new Rect(Screen.width / 2f, Screen.height / 2f, 250, 120);
+            var window = GetWindow<ChangeCellSizeWindow>();
+            
             // Showing it as a modal forces the user to process it immediately
             window.ShowModalUtility();
         }
-    
+
         void OnGUI()
         {
+            GUI.SetNextControlName(cellSizeControlName);
             newCellSize = EditorGUILayout.Vector2IntField("Cell Size", newCellSize);
-        
+
+            if (!cellSizeControlFocused)
+            {
+                EditorGUI.FocusTextInControl(cellSizeControlName);
+                cellSizeControlFocused = true;
+            }
+            
             var wrongCellSize = (newCellSize.x <= 0 || newCellSize.y <= 0);
 
             if (wrongCellSize)
@@ -36,12 +47,28 @@ namespace Array2DEditor
 
             if (GUILayout.Button("Apply"))
             {
-                cellSizeProperty.vector2IntValue = newCellSize;
-                cellSizeProperty.serializedObject.ApplyModifiedProperties();
-                Close();
+                Apply();
             }
 
             GUI.enabled = true;
+
+            // We're doing this in OnGUI() since the Update() function doesn't seem to get called when we show the window with ShowModalUtility().
+            if ((Event.current.type == EventType.KeyDown || Event.current.type == EventType.KeyUp) &&
+                (Event.current.keyCode == KeyCode.Return || Event.current.keyCode == KeyCode.KeypadEnter))
+            {
+                Apply();
+            }
+        }
+
+        private void Apply()
+        {
+            if (newCellSize != cellSizeProperty.vector2IntValue)
+            {
+                cellSizeProperty.vector2IntValue = newCellSize;
+                cellSizeProperty.serializedObject.ApplyModifiedProperties();
+            }
+            
+            Close();
         }
     }
 }
