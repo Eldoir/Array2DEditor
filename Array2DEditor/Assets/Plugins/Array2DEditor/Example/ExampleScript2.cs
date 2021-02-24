6f25ba410cbcd6444e711dc524b5a3faa5cccd5f
Arthur Cousseau <arthur.cousseau@me.com>
2021-02-25 00:26:59 +0100

    Use ExampleScript2 in ExampleScene

---
+++ Array2DEditor/Assets/Plugins/Array2DEditor/Example/ExampleScript2.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Array2DEditor
+{
+    public class ExampleScript2 : MonoBehaviour
+    {
+        [SerializeField]
+        private Array2DInt arrayInt;
+
+        [SerializeField]
+        private Array2DExampleEnum arrayEnum;
+
+        [SerializeField]
+        private Array2DBool arrayBool;
+
+        [SerializeField]
+        private Array2DFloat arrayFloat;
+
+        [SerializeField]
+        private Array2DString arrayString;
+    }
+}
