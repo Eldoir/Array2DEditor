790970dad79c75cbd77802dd086a4c85100c6903
Arthur Cousseau <arthur.cousseau@me.com>
2023-07-24 17:57:58 +0200

    Make CellRow<T> implement IEnumerable<T>

--- Array2DEditor/Assets/Plugins/Array2DEditor/Scripts/CellRow.cs
+++ Array2DEditor/Assets/Plugins/Array2DEditor/Scripts/CellRow.cs
@@ -1,9 +1,11 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace Array2DEditor
 {
     [System.Serializable]
-    public class CellRow<T>
+    public class CellRow<T> : IEnumerable<T>
     {
         [SerializeField]
         private T[] row = new T[Consts.defaultGridSize];
@@ -13,5 +15,22 @@ namespace Array2DEditor
             get => row[i];
             set => row[i] = value;
         }
+
+        #region IEnumerable
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            for (int i = 0; i < row.Length; i++)
+            {
+                yield return this[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<T>)this).GetEnumerator();
+        }
+
+        #endregion
     }
 }
